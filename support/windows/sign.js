44f1b0f91e1c6506ea5c7979523d7e04a4b8bc5c
Caleb Woodbine <calebwoodbine.public@gmail.com>
2018-12-06 14:24:43 +1300

    UPDATED: package.json, sign.js
    Added:
    - sign.js, to handle sign commands
    
    Removed:
    - build-appx.js, as it's unneeded

---
+++ support/windows/sign.js
@@ -0,0 +1,59 @@
+// SafeSurfer-Desktop - sign.js
+
+//
+// Copyright (C) 2018 Caleb Woodbine <info@safesurfer.co.nz>
+//
+// This file is part of SafeSurfer-Desktop.
+//
+// SafeSurfer-Desktop is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// SafeSurfer-Desktop is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with SafeSurfer-Desktop.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+/*
+  a small js file for signing Windows binaries
+*/
+
+const exec = require('child_process').execSync,
+  fs = require('fs')
+  args = process.argv.splice(2, process.argv.length),
+  path = require('path');
+
+  if (os.platform() === 'win32') {
+    // get content of .electron-windows-store file
+    const ewsCfg = JSON.parse(fs.readFileSync(path.join(process.env['USERPROFILE'], '.electron-windows-store')), 'utf8');
+  }
+  else {
+    const publicKey = path.resolve(process.env['winsign_publicKey']),
+      privateKey = path.resolve(process.env['winsign_privateKey']);
+  }
+
+const binary = args[0];
+// if no arg is passed
+if (binary === undefined) {
+  console.log(`Please enter a file to sign.`);
+  process.exit(1);
+}
+binary = path.resolve(args[0]);
+// determine if the file exists
+if (fs.existsSync(binary) !== true) {
+    console.log(`File ${binary} not found.`);
+    process.exit(1);
+}
+
+if (os.platform() === 'win32') {
+  // call generated command
+  exec(`cd ${ewsCfg.windowsKit} && signtool.exe sign /f ${ewsCfg.devCert} ${binary}`);
+}
+else {
+  exec(`signcode -spc ${publicKey} -v ${privateKey} -a sha1 -$ commercial -n SafeSurfer-Desktop -i https://safesurfer.co.nz/ -t http://timestamp.verisign.com/scripts/timstamp.dll -tr 10 ${binary}`)
+}
